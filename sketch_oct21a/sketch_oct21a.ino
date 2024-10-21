d9b5372d94185c784f22d9aeb84c74e11987a6d3
Ted Choi <74972091+Ted0007-123@users.noreply.github.com>
2024-10-21 12:34:03 -0400

    Update sketch_oct21a.ino

--- sketch_oct21a/sketch_oct21a.ino
+++ sketch_oct21a/sketch_oct21a.ino
@@ -1,12 +1,19 @@
-#define Weight_Tolerance = 0.9
+const int FSRPin = A0;
+
+const int Weight_Tolerance = 0.9;
+const float Vcc = 3.3;
+float fsrVoltage;
+float fsrResistance;
+float weight;
+float A = 100.0;              
+float B = -1.5;
+int Max_Weight = 0;
+
 void setup() {
-  // put your setup code here, to run once:
-  int Max_Weight = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Max_Weight > calculate_Weight()* Weight_Tolerance && isAuthorized()){
+  if(Max_Weight > calculate_Weight()* Weight_Tolerance){
     sound_Alarm();
   }else if(Max_Weight < calculate_Weight()){
     Max_Weight = calculate_Weight();
@@ -14,11 +21,14 @@ void loop() {
 }
 
 int calculate_Weight(){
-
+  int FSRVal_Analog = analogRead(FSRPin);
+  FSRVoltage = (FSRVal_Analog/1023.0)*Vcc;
+  if(FSRVoltage > 0){
+    R_FSR = R_known * (Vcc - FSRVoltage) / FSRVoltage;
+    weight = A*pow(R_FSR, B)
+  }
+  return weight;
 }
 void sound_Alarm(){
-
-}
-bool isAuthorized(){
 
 }
