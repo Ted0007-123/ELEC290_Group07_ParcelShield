cf7b56b56e81295804adde9facb0df0b64de8574
Ted Choi <74972091+Ted0007-123@users.noreply.github.com>
2024-10-21 12:15:17 -0400

    Update sketch_oct21a.ino

--- sketch_oct21a/sketch_oct21a.ino
+++ sketch_oct21a/sketch_oct21a.ino
@@ -1,9 +1,24 @@
+#define Weight_Tolerance = 0.9
 void setup() {
   // put your setup code here, to run once:
-
+  int Max_Weight = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(Max_Weight > calculate_Weight()* Weight_Tolerance && isAuthorized()){
+    sound_Alarm();
+  }else if(Max_Weight < calculate_Weight()){
+    Max_Weight = calculate_Weight();
+  }
+}
+
+int calculate_Weight(){
+
+}
+void sound_Alarm(){
+
+}
+bool isAuthorized(){
 
 }
