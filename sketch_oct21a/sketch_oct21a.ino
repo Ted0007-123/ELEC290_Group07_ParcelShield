b1c8104adc2dcdf93c63e9c8983cd29db89694c9
Ted Choi <74972091+Ted0007-123@users.noreply.github.com>
2024-10-24 21:13:23 -0400

    Update sketch_oct21a.ino

--- sketch_oct21a/sketch_oct21a.ino
+++ sketch_oct21a/sketch_oct21a.ino
@@ -8,6 +8,37 @@ float weight;
 float A = 100.0;              
 float B = -1.5;
 int Max_Weight = 0;
+const long Password_Master = 20001012;
+
+typedef struct PasswordNode{
+  long Password;
+  struct PasswordNode* next;
+}PasswordNode;
+
+PasswordNode* password_list_head = NULL;
+
+
+long createPassword(){
+  long pwd = random(99999999);
+  PasswordNode* new_password = (PasswordNode*)malloc(sizeof(PasswordNode));
+  new_password -> Password = pwd;
+  new_password -> next = password_list_head;
+  password_list_head = new_password;
+  return pwd;
+}
+
+int is_Password_remove(struct PasswordNode** head_ref, long pwd){
+  if(*head_ref==NULL){
+    return 0;
+  }
+  if((*head_ref) -> Pwassword == pwd){
+    struct PasswordNode* temp = *head_ref;
+    *head_ref = (*head_ref)-> next;
+    free(temp);
+    return 1;
+  }
+  is_Password_remove(&((*head_ref)->next), pwd);
+}
 
 void setup() {
 }
