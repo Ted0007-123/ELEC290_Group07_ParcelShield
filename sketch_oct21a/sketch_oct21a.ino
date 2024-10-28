c214ad952d4f62f1bc3a4adc8cfd72785632332c
Ted Choi <74972091+Ted0007-123@users.noreply.github.com>
2024-10-28 16:15:51 -0400

    keypad and weight measuring update

--- sketch_oct21a/sketch_oct21a.ino
+++ sketch_oct21a/sketch_oct21a.ino
@@ -3,17 +3,18 @@ const int FSRPin = A0;
 
 const int Weight_Tolerance = 0.9;
 const float Vcc = 3.3;
+const float knownResistance = 12000;
 float fsrVoltage;
 float fsrResistance;
-float weight;
-float A = 100.0;              
-float B = -1.5;
+float mass;
+float A = 271.0;              
+float B = -0.69;
 int Max_Weight = 0;
 
-byte pin_rows[ROW_NUM] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {5, 4, 3}; //connect to the column pinouts of the keypad
+byte pin_rows[4] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte pin_column[3] = {5, 4, 3}; //connect to the column pinouts of the keypad
 
-char keys[ROW_NUM][COLUMN_NUM] = {
+char keys[4][3] = {
   {'1','2','3'},
   {'4','5','6'},
   {'7','8','9'},
@@ -31,14 +32,14 @@ void loop() {
   }
 }
 
-int calculate_Weight(){
-  int FSRVal_Analog = analogRead(FSRPin);
-  FSRVoltage = (FSRVal_Analog/1023.0)*Vcc;
-  if(FSRVoltage > 0){
-    R_FSR = R_known * (Vcc - FSRVoltage) / FSRVoltage;
-    weight = A*pow(R_FSR, B)
-  }
-  return weight;
+float calculate_Weight(){
+  fsrVoltage = (analogRead(A0)*Vcc/1023);
+  fsrResistance = resistance_calculation(fsrVoltage);
+  fsrResistance /= 1000; //into kilo ohms
+  mass = pow((fsrResistance/A),1/B)/9.81;
+}
+float resistance_calculation(float voltage_in){
+  return (knownResistance * (Vcc - voltage_in )/voltage_in);
 }
 void sound_Alarm(){
 
